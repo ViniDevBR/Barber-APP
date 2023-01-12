4424083dbe4f63515cf296dfa47ad8fd9347c263
ViniDevBR <vinigtr386@gmail.com>
2023-01-12 13:25:30 -0300

    :fire: Starting web and all configs files

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/globalStyles.css'
+
+export function App() {
+  const linkWhatsApp = 'https://wa.me/5513996871913?text=Ol%C3%A1%2C+vim+atrav%C3%A9s+do+site+e+tenho+algumas+duvidas.'
+
+  return (
+    <div className="flex rounded-sm">
+
+    </div>
+  )
+}
