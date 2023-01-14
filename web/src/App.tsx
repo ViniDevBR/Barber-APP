6f77ead9471e100539e5e6aaa25c6d15ee1d9419
ViniDevBR <vinigtr386@gmail.com>
2023-01-14 19:04:32 -0300

    :fire: Using Tailwind components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,20 @@
 import './styles/globalStyles.css'
 import logo from './assets/logo.png'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from '@material-tailwind/react'
 import { WhatsApp } from './components/WhatsApp'
+import { IClient } from './@types/Clients'
+import { clientsTest } from './clients'
 
 export function App() {
-  const [listOrNew, setListOrNew] = useState<boolean>(false)
-
+  const [clients, setClients] = useState<IClient[]>([])
+  const x = 5
   const hour = new Date().getHours()
   const Hour = () => {
     if (hour >= 0 && hour < 6) {
@@ -22,22 +31,46 @@ export function App() {
     }
   }
 
+  useEffect(() => {
+    setClients(clientsTest)
+  },[])
+
   return (
-    <div className='bg-gray-950 w-full h-screen'>
+    <div className='bg-gray-950 w-full min-h-screen'>
       <header className='flex items-center justify-between border-b border-b-gray-500 px-5 py-3'>
-        <img src={logo} alt="Logo da barbearia" className='w-[105px]'/>
+        <img src={logo} alt='Logo da barbearia' className='w-[105px]'/>
         <span className='text-white text-xl font-bold'>{Hour()}</span>
       </header>
 
       <main className='w-full px-5 py-4'>
-        <div onClick={() => setListOrNew(!listOrNew)} className='bg-gray-350 w-full h-6 rounded-lg'>
-          {listOrNew && (
-            <div></div>
-          )}
-          {!listOrNew && (
-            <div></div>
-          )}
-        </div>
+        <Tabs value='fila'>
+          <TabsHeader className='bg-gray-350 bg-opacity-100 p-0 rounded'>
+            <Tab value='fila' >
+              <span className='font-bold text-xs'>Fila de espera</span>
+            </Tab>
+            <Tab value='entrar'>
+              <span className='font-bold text-xs'>Entrar na fila</span>
+            </Tab>
+          </TabsHeader>
+          <TabsBody>
+            <TabPanel value='fila' className='p-0 pt-4 text-blue-gray-200'>
+              <span className='text-xs font-thin'>{x} Pessoas na fila</span>
+
+              {clients.map((client, index) => {
+                const position = index + 1
+
+                return(
+                  <div key={client.id} className='flex items-center justify-between w-full bg-gray-350 py-2 px-4 rounded mb-2 mt-1'>
+                    <span className='text-gray-950'>{position}°</span>
+                    <p className='text-gray-950 '>{client.name}</p>
+                  </div>
+                )})}
+            </TabPanel>
+            <TabPanel value='entrar'>
+            OLA MUNDO 2
+            </TabPanel>
+          </TabsBody>
+        </Tabs>
       </main>
 
       <WhatsApp />
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,19 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
+const withMT = require('@material-tailwind/react/utils/withMT')
 
-module.exports = {
+module.exports = withMT({
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
       colors: {
         gray: {
           350: '#D8D8D8',
+          550: '#6F6F6F',
           950: '#151515'
         },
         green: {
           450: '#00B929'
         }
       }
-    },
+    }
   },
-  plugins: [],
-}
+  plugins: []
+})
