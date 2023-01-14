f9dbf772266a29ec0a7be1a54afca786da5e0bdf
ViniDevBR <vinigtr386@gmail.com>
2023-01-13 22:51:13 -0300

    :fire: New lib, colors for tailwind and title HTMl

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -3,7 +3,17 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          350: '#D8D8D8',
+          950: '#151515'
+        },
+        green: {
+          450: '#00B929'
+        }
+      }
+    },
   },
   plugins: [],
 }
