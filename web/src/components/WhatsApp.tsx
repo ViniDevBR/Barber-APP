1bcbfb8eb9687d41115e3fa0d7ebc49bacc40cfa
ViniDevBR <vinigtr386@gmail.com>
2023-01-17 23:10:04 -0300

    :fire: FINISH WEB

--- web/src/components/WhatsApp.tsx
+++ web/src/components/WhatsApp.tsx
@@ -4,8 +4,8 @@ export function WhatsApp() {
   const linkWhatsApp = 'https://wa.me/5513996871913?text=Ol%C3%A1%2C+vim+atrav%C3%A9s+do+site+e+tenho+algumas+duvidas.'
 
   return (
-    <a href={linkWhatsApp} className='bg-green-450 rounded-full w-12 h-12 flex items-center justify-center fixed bottom-4 right-4 z-10' >
-      <WhatsappLogo size={35} color='#fff' weight='thin' />
+    <a href={linkWhatsApp} className='bg-blue-gray-100 rounded-full w-12 h-12 flex items-center justify-center fixed bottom-4 right-4 z-10' >
+      <WhatsappLogo size={35} color='#00B929' weight='duotone' />
     </a>
   )
 }
